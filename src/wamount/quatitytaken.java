3cc198c92f12316a07196a56d03e0d8e6702e88e
milkaotieno <milka.otieno@student.moringaschool.com>
2022-04-14 17:03:13 +0300

    added comments

--- src/wamount/quatitytaken.java
+++ src/wamount/quatitytaken.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
 
 import java.util.List;
 import java.util.Scanner;
-
+//Write a function to return total amount of water
 public class quatitytaken {
     public static int main(Struct.String[] args) {
-        double water = 0.5;
+        double water = 0.5;//amount of water taken per hour
         double product;
-        double time = 0;
-        product = (time * 0.5);
+        double time = 0; // total hours taken while cycling
+        product = (time * 0.5); // quantity of water taken while cycling
         return (int) product;
 
     }
