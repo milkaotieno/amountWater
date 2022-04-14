90974a2f2a5a685fdd9b577c552a906f931ca349
milkaotieno <milka.otieno@student.moringaschool.com>
2022-04-14 16:59:13 +0300

    Initial commit

---
+++ src/wamount/quatitytaken.java
@@ -0,0 +1,20 @@
+package wamount;
+
+import jnr.ffi.Struct;
+import java.util.Scanner;
+
+
+import java.util.List;
+import java.util.Scanner;
+
+public class quatitytaken {
+    public static int main(Struct.String[] args) {
+        double water = 0.5;
+        double product;
+        double time = 0;
+        product = (time * 0.5);
+        return (int) product;
+
+    }
+ System.out.println(product);
+}
